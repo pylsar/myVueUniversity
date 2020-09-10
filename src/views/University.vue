7eb48611e53af89d7dbacf5c9d93c77325b9b121
pylsar <kysov2006@bk.ru>
2020-09-10 07:22:06 +0300

    University components add links styles popup start

--- src/views/University.vue
+++ src/views/University.vue
@@ -2,14 +2,16 @@
     <div class="university">
         <h1>{{university.title}}</h1>
         <div class="university__box">
-            <div class="university__box__item">
-                <div class="university__box__item--link">
-                    <!-- ? -->
-                    <span @click="excursionRedirect">Заказать экскурсию</span>
+            <div class="university__box__item university__box__left">
+                <div class="university__box__item--summary" @click="showPopup">click</div>
+                <div class="university__box__item__link">
+                    <router-link to='/excursion'>Заказать экскурсию</router-link>
                 </div>
-                <div class="university__box__item--link">
-                    <!-- ? -->
-                    <span @click="commentsRedirect">Комментарии</span>
+                <div class="university__box__item__link">
+                    <router-link to='/comments'>Комментарии</router-link>
+                </div>
+                <div class="university__box__item__link">
+                    <router-link to='/gallary'>Галерея</router-link>
                 </div>
             </div>
             <div class="university__box__item">
@@ -17,9 +19,9 @@
                 <img  :src="require('../assets/img/' + university.img)" alt="university.title" />
                 </div>
             </div>
-            
         </div>
         <p>{{university.longDescr}}</p>
+        <div v-show="popupShow">{{university.title}}</div>
     </div>
 </template>
 <script>
@@ -28,6 +30,7 @@ import {mapGetters, mapActions} from 'vuex'
         name: 'University',
         data() {
             return {
+                popupShow: false
             };
         },
        computed:{
@@ -43,8 +46,11 @@ import {mapGetters, mapActions} from 'vuex'
             ...mapActions([
                 'GET_UNIVERSITIES_FROM_API',
             ]),
-            excursionRedirect(){
-                this.$router.push({ name: 'Excursion' })
+            // excursionRedirect(){
+            //     this.$router.push({ name: 'Excursion' })
+            // },
+            showPopup(){
+                this.popupShow = !this.popupShow
             }
         },
         mounted(){
@@ -61,6 +67,7 @@ import {mapGetters, mapActions} from 'vuex'
             margin-top: 20px;
         }
         &__box {
+            position: relative;
             display: flex;
             align-items: center;
             justify-content: center;
@@ -69,8 +76,21 @@ import {mapGetters, mapActions} from 'vuex'
                 width: 50%;
                 display: flex;
                 justify-content: center;
-                &--link{
+                &__link{
+                    margin-bottom: 10px;
+                    width: 200px;
+                    height: 50px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background: skyblue;
                     border: 1px solid orange;
+                    border-radius: 5px;
+                }
+                &--summary{
+                    position: absolute;
+                    top: 0;
+                    left: 0;
                 }
                 &--img {
                     border: 1px solid blue;
@@ -78,6 +98,13 @@ import {mapGetters, mapActions} from 'vuex'
                         max-width: 100%;
                     }
                 }    
+            }
+            &__left {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-around;
+                align-items: center;
+                width: 50%;
             }
         }
         
